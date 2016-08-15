d826e053c747eccef3b5f90a664ebd1f0471cce4
johnsmith1st <johnsmith1st@live.com>
2016-08-15 10:50:17 +0800

    add expro json error

--- test/test-expro-json.js
+++ test/test-expro-json.js
@@ -6,78 +6,6 @@ const expro = require('../lib');
 
 const MockResponse = require('./test-mocks').MockResponse;
 
-// describe('expro-json', function () {
-//
-//   describe('.exproJsonErrorMiddleware', function () {
-//
-//     function test(args, expectedStatus, expectedJson, done) {
-//
-//       let res = new MockResponse();
-//
-//       exproJsonErrorMiddleware(null, res, () => {
-//         try {
-//           expect(res.expro.jsonError).to.be.a('function');
-//           res.expro.jsonError.apply(this, args);
-//           expect(res._status).to.equal(expectedStatus);
-//           expect(res._json).to.deep.equal(expectedJson);
-//           done();
-//         }
-//         catch(err) {
-//           done(err);
-//         }
-//       });
-//     }
-//
-//     it('should be ok', function (done) {
-//       test([ Error('oops') ], 500, {
-//         code: 500,
-//         error: { code: undefined, message: 'oops' }
-//       }, done);
-//     });
-//
-//     it('should be ok with args statusCode', function (done) {
-//       test([ Error('oops'), 400 ], 400, {
-//         code: 400,
-//         error: { code: undefined, message: 'oops' }
-//       }, done);
-//     });
-//
-//     it('should be ok with error.statusCode', function (done) {
-//       let err = Object.assign(Error('oops'), { statusCode: 400 });
-//       test([ err, 200 ], 400, {
-//         code: 400,
-//         error: { code: undefined, message: 'oops' }
-//       }, done);
-//     });
-//
-//     it('should be ok with error.status', function (done) {
-//       let err = Object.assign(Error('oops'), { status: 400 });
-//       test([ err, 200 ], 400, {
-//         code: 400,
-//         error: { code: undefined, message: 'oops' }
-//       }, done);
-//     });
-//
-//     it('should be ok with error.errorCode', function (done) {
-//       let err = Object.assign(Error('oops'), { errorCode: 'BAD_REQUEST' });
-//       test([ err, 400 ], 400, {
-//         code: 400,
-//         error: { code: 'BAD_REQUEST', message: 'oops' }
-//       }, done);
-//     });
-//
-//     it('should be ok with error.code', function (done) {
-//       let err = Object.assign(Error('oops'), { code: 'BAD_REQUEST' });
-//       test([ err, 400 ], 400, {
-//         code: 400,
-//         error: { code: 'BAD_REQUEST', message: 'oops' }
-//       }, done);
-//     });
-//
-//   });
-//
-// });
-
 describe('expro.jsonResult()', function () {
 
   it('should create a middleware', function () {
@@ -137,6 +65,72 @@ describe('expro.jsonResult()', function () {
     expro.jsonResult()(null, res, () => {
       done(Error('should not go here'));
     });
+  });
+
+});
+
+describe('expro.jsonError()', function() {
+
+  function test(err, expStatusCode, expErrorCode, expMessage, done) {
+
+    let res = new MockResponse();
+
+    res.on('end', () => {
+      try {
+        expect(res._status).to.equal(expStatusCode);
+        expect(res._json).to.deep.equal({
+          code: expStatusCode,
+          error: {
+            code: expErrorCode,
+            message: expMessage
+          }
+        });
+        done();
+      }
+      catch(err) {
+        done(err);
+      }
+    });
+
+    expro.jsonError()(err, null, res, () => {
+      done(Error('should not go here'));
+    });
+  }
+
+  it('should create a middleware', function () {
+    expect(expro.jsonResult()).to.be.a('function');
+  });
+
+  it('should send error as json', function (done) {
+    test(Error('oops'), 500, undefined, 'oops', done);
+  });
+
+  it('should send error with status', function (done) {
+    let err = Error('oops');
+    err.status = 400;
+    test(err, 400, undefined, 'oops', done);
+  });
+
+  it('should send error with status code', function (done) {
+    let err = Error('oops');
+    err.statusCode = 400;
+    err.status = 500;
+    test(err, 400, undefined, 'oops', done);
+  });
+
+  it('should send error with code', function (done) {
+    let err = Error('oops');
+    err.status = 400;
+    err.code = 'BAD_REQUEST';
+    test(err, 400, 'BAD_REQUEST', 'oops', done);
+  });
+
+  it('should send error with error code', function (done) {
+    let err = Error('oops');
+    err.status = 403;
+    err.code = 'BAD_REQUEST';
+    err.errorCode = 'FORBIDDEN';
+    test(err, 403, 'FORBIDDEN', 'oops', done);
   });
 
 });
