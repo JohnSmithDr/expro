64948485655ba417e3a1ef17321e46b2116fa684
johnsmith1st <johnsmith1st@live.com>
2016-08-15 15:48:48 +0800

    add to boolean converter

--- test/test-expro-converters.js
+++ test/test-expro-converters.js
@@ -202,3 +202,134 @@ describe('expro.ctx.prop().toNumber()', function () {
   });
 
 });
+
+describe('expro.ctx.prop().toBool()', function () {
+
+  it('should be ok', function (done) {
+
+    let cases = [
+      ['true', true],
+      ['True', true],
+      ['TRUE', true],
+      ['false', false],
+      ['False', false],
+      ['FALSE', false],
+      ['yes', true],
+      ['Yes', true],
+      ['YES', true],
+      ['no', false],
+      ['No', false],
+      ['NO', false],
+      ['tru', false],
+      ['fal', false],
+      ['1', true],
+      ['10', true],
+      ['+1', true],
+      ['-1', true],
+      ['0', false],
+      [1, true],
+      [10, true],
+      [+1, true],
+      [-1, true],
+      [0, false]
+    ];
+
+    let test = (i, value, expected, next) => {
+
+      let req = new MockRequest({ query: { foo: value } });
+
+      let mw = expro.query.prop('foo').toBool();
+
+      expect(mw).to.be.a('function');
+
+      mw(req, {}, () => {
+        try {
+          expect(req.query.foo).to.equal(expected);
+          next(0, ++i);
+        }
+        catch(err) {
+          console.error('# %s # value: %s, expected: %s', i, value, expected);
+          next(err);
+        }
+      });
+
+    };
+
+    let next = (err, i) => {
+
+      if (err) return done(err);
+      if (i >= cases.length) return done();
+
+      let d = cases[i];
+      test(i, d[0], d[1], next);
+
+    };
+
+    next(0, 0);
+
+  });
+
+  it('should be ok if prop is undefined', function (done) {
+
+    let req = new MockRequest({ query: { } });
+
+    let mw = expro.query.prop('foo').toBool();
+
+    expect(mw).to.be.a('function');
+
+    mw(req, {}, () => {
+      try {
+        expect(req.query).to.not.have.property('foo');
+        done();
+      }
+      catch(err) {
+        done(err);
+      }
+
+    });
+
+  });
+
+  it('should be ok if prop is not a boolean value', function (done) {
+
+    let req = new MockRequest({ query: { foo: 'xxx' } });
+
+    let mw = expro.query.prop('foo').toBool();
+
+    expect(mw).to.be.a('function');
+
+    mw(req, {}, () => {
+      try {
+        expect(req.query.foo).to.equal(false);
+        done();
+      }
+      catch(err) {
+        done(err);
+      }
+
+    });
+
+  });
+
+  it('should be ok with default value', function (done) {
+
+    let req = new MockRequest({ query: { foo: 'xxx' } });
+
+    let mw = expro.query.prop('foo').toBool(true);
+
+    expect(mw).to.be.a('function');
+
+    mw(req, {}, () => {
+      try {
+        expect(req.query.foo).to.equal(true);
+        done();
+      }
+      catch(err) {
+        done(err);
+      }
+
+    });
+
+  });
+
+});
