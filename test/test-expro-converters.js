f8a5ccee10bba684dc068e91f3fb6f03b6860a0e
johnsmith1st <johnsmith1st@live.com>
2016-08-15 13:15:22 +0800

    fix default value

--- test/test-expro-converters.js
+++ test/test-expro-converters.js
@@ -48,6 +48,49 @@ describe('expro.ctx.prop().toInt()', function () {
 
   });
 
+  it('should be ok if prop is undefined', function (done) {
+
+    let req = new MockRequest({ query: { foo: '100' } });
+
+    let mw = expro.query.prop('bar').toInt();
+
+    expect(mw).to.be.a('function');
+
+    mw(req, {}, () => {
+      try {
+        expect(req.query.foo).to.equal('100');
+        expect(req.query).to.not.have.property('bar');
+        done();
+      }
+      catch(err) {
+        done(err);
+      }
+
+    });
+
+  });
+
+  it('should be ok if prop is not a numeric value', function (done) {
+
+    let req = new MockRequest({ query: { foo: 'xxx' } });
+
+    let mw = expro.query.prop('foo').toInt();
+
+    expect(mw).to.be.a('function');
+
+    mw(req, {}, () => {
+      try {
+        expect(req.query.foo).to.equal(0);
+        done();
+      }
+      catch(err) {
+        done(err);
+      }
+
+    });
+
+  });
+
   it('should be ok with default value', function (done) {
 
     let req = new MockRequest({ query: { foo: 'xxx' } });
@@ -84,6 +127,49 @@ describe('expro.ctx.prop().toNumber()', function () {
     mw(req, {}, () => {
       try {
         expect(req.query.foo).to.equal(1.1);
+        done();
+      }
+      catch(err) {
+        done(err);
+      }
+
+    });
+
+  });
+
+  it('should be ok if prop is undefined', function (done) {
+
+    let req = new MockRequest({ query: { foo: '100' } });
+
+    let mw = expro.query.prop('bar').toNumber();
+
+    expect(mw).to.be.a('function');
+
+    mw(req, {}, () => {
+      try {
+        expect(req.query.foo).to.equal('100');
+        expect(req.query).to.not.have.property('bar');
+        done();
+      }
+      catch(err) {
+        done(err);
+      }
+
+    });
+
+  });
+
+  it('should be ok if prop is not a numeric value', function (done) {
+
+    let req = new MockRequest({ query: { foo: 'xxx' } });
+
+    let mw = expro.query.prop('foo').toNumber();
+
+    expect(mw).to.be.a('function');
+
+    mw(req, {}, () => {
+      try {
+        expect(req.query.foo).to.equal(0);
         done();
       }
       catch(err) {
