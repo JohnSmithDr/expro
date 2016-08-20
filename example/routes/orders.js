7f17492de1c02055e521ab5ba9fe272920e4404c
johnsmith1st <johnsmith1st@live.com>
2016-08-20 23:55:41 +0800

    update example orders api

--- example/routes/orders.js
+++ example/routes/orders.js
@@ -8,23 +8,40 @@ module.exports.createOrder = expro(
   expro
     .with(x => x.body)
     .await(ordersService.createOrder),
+  expro.wrap('order'),
   expro.status(201)
 );
 
 module.exports.deleteOrder = expro(
   expro
-    .with(x => x.swagger.params.orderId.value)
-    .await(ordersService.deleteOrder)
+    .with(x => x.swagger.params['orderId'].value)
+    .await(ordersService.deleteOrder),
+  expro.wrap('order')
 );
 
 module.exports.getOrder = expro(
   expro
-    .with(x => x.swagger.params.orderId.value)
-    .await(ordersService.getOrder)
+    .with(x => x.swagger.params['orderId'].value)
+    .await(ordersService.getOrder),
+  expro.wrap('order')
 );
 
 module.exports.queryOrders = expro(
   expro
     .with(x => x.query)
     .await(ordersService.queryOrders)
+);
+
+module.exports.updateOrderShippingData = expro(
+  expro
+    .with(x => x.swagger.params['orderId'].value, x => x.body.shippingDate)
+    .await(ordersService.updateOrderShippingDate),
+  expro.wrap('order')
+);
+
+module.exports.updateOrderStatus = expro(
+  expro
+    .with(x => x.swagger.params['orderId'].value, x => x.body.status)
+    .await(ordersService.updateOrderStatus),
+  expro.wrap('order')
 );
--- example/services/orders-service.js
+++ example/services/orders-service.js
@@ -1,19 +1,57 @@
 'use strict';
 
+const dataSource = require('../data/data-source');
+const makeReject = require('../errors').makeReject;
+
 function createOrder(params) {
-  return Promise.resolve(params);
+  return dataSource.orders.insertOrUpdate(params);
 }
 
 function deleteOrder(orderId) {
-  return Promise.resolve(orderId);
+  return dataSource.orders.findAndRemoveById(orderId)
+    .then(order => {
+      return order
+        ? Promise.resolve(order)
+        : makeReject(404, 'ORDER_NOT_FOUND', `Order not found: ${orderId}`);
+    });
 }
 
 function getOrder(orderId) {
-  return Promise.resolve(orderId);
+  return dataSource.orders.findById(orderId)
+    .then(order => {
+      return order
+        ? Promise.resolve(order)
+        : makeReject(404, 'ORDER_NOT_FOUND', `Order not found: ${orderId}`);
+    });
 }
 
 function queryOrders(query) {
   return Promise.resolve(query);
 }
 
-module.exports = { createOrder, deleteOrder, getOrder, queryOrders };
+function updateOrderShippingDate(orderId, date) {
+  return dataSource.orders.findById(orderId)
+    .then(order => {
+      if (!order) return makeReject(404, 'ORDER_NOT_FOUND', `Order not found: ${orderId}`);
+      order.shippingDate = shippingDate;
+      return dataSource.orders.insertOrUpdate(order);
+    });
+}
+
+function updateOrderStatus(orderId, status) {
+  return dataSource.orders.findById(orderId)
+    .then(order => {
+      if (!order) return makeReject(404, 'ORDER_NOT_FOUND', `Order not found: ${orderId}`);
+      order.status = status;
+      return dataSource.orders.insertOrUpdate(order);
+    });
+}
+
+module.exports = {
+  createOrder,
+  deleteOrder,
+  getOrder,
+  queryOrders,
+  updateOrderShippingDate,
+  updateOrderStatus
+};
